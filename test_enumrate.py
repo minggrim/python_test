f4df6a4eb07ae40ab0c3235a58f7800c66c31c8d
liam.liao <liam.liao@latticeworkinc.com>
2017-02-16 14:33:37 +0800

    [enumrate]

---
+++ test_enumrate.py
@@ -0,0 +1,11 @@
+my_list = ['a', 'b', 'c']
+
+for i, element in enumerate(my_list):
+    print(i)
+    print(element)
+
+i = 0
+while i < len(my_list):
+    print(i)
+    print(my_list[i])
+    i = i + 1
